c35d72ae975e85e0cab49fdeb146994f4d7fcfb3
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-02-27 13:23:22 +0200

    java backend server is running

--- backend/src/main/java/com/example/api/repository/AdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.repository;
-
-import com.example.api.model.Ad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdRepository extends JpaRepository<Ad, Long> {
-}
